99e29619218636a00402121e860c17ce741197e6
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-01 01:42:31 -0300

    fix: tests

--- src/organizations/management/commands/load_initial_data.py
+++ src/organizations/management/commands/load_initial_data.py
@@ -314,7 +314,7 @@ class Command(BaseCommand):
                     key__in=measures_keys,
                 )
 
-                if measures.count() != len(measures_keys):
+                if measures.count() < len(measures_keys):
                     raise exceptions.MissingSupportedMeasureException()
 
                 sub_char.measures.set(measures)
