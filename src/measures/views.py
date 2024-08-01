9a6fd5a4342461964c05796fc9cf2b48126f2b97
Christian Siqueira <chfleurysiq@gmail.com>
2024-08-01 10:57:56 -0300

    fix: typeerror

--- src/measures/views.py
+++ src/measures/views.py
@@ -77,7 +77,7 @@ class CalculateMeasuresViewSet(
                 core_params['measures'].append(
                     {
                         'key': measure.key,
-                        'metrics': [{"key": key, "value": [float(value)]} for key, value in metric_params.items()],
+                        'metrics': [{"key": key, "value": value} for key, value in metric_params.items()],
                     }
                 )
         # 5. Pega as configurações das thresholds
